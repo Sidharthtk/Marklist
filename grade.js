ab519cd8a24e6b30cc83ab7ca98f07d39163571f
sidharth <sidharthsidhutk@gmail.com>
2024-09-20 16:11:26 +0530

    create marklist

---
+++ grade.js
@@ -0,0 +1,20 @@
+const grade=75
+if(grade>=90){
+    console.log("A+");
+    
+}else if(grade>=80&&grade<90){
+    console.log("A");
+    
+}else if(grade>=70&&grade<80){
+    console.log("B+");
+    
+}else if(grade>=60&&grade<70){
+    console.log("B");
+    
+}else if(grade>=50&&grade<60){
+    console.log("C");
+    
+}else if(grade>=0&&grade<50){
+    console.log("F");
+    
+}
